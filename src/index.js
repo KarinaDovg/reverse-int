08f2df2482a779ee70ab01b3a31725f12408426a
KarinaDovg <korzhenevskaya.2020@mail.ru>
2023-08-05 13:03:34 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse (n) {
-  
+  return n > 0 ? Number(n.toString().split('').reverse().join('').toString()) : Number((n / (-1)).toString().split('').reverse().join('').toString());
 }
